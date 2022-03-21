c4a52cb81cb0e13b5ca0c1d3b11ff02ae73ca06e
sj <sijiaw@yahoo.com>
2022-03-21 06:35:11 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import SelectCharacter from './Components/SelectCharacter';
+import Arena from './Components/Arena';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import twitterLogo from './assets/twitter-logo.svg';
+import myEpicGame from './utils/MyEpicGame.json';
+import { ethers } from 'ethers';
+import LoadingIndicator from './Components/LoadingIndicator';
+
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // State
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        /*
+         * We set isLoading here because we use return in the next line
+         */
+        setIsLoading(false);
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    /*
+     * We release the state property after all the function logic
+     */
+    setIsLoading(false);
+};
+
+  // Render Methods
+const renderContent = () => {
+  /*
+   * If the app is currently loading, just render out LoadingIndicator
+   */
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (!currentAccount) {
+    return (
+      <div className="connect-wallet-container">
+        <img
+          src="https://www.letseatcake.com/wp-content/uploads/2022/02/Tinder-Swindler-Tweets-4.jpg"
+          alt="Gif"
+        />
+        <button
+          className="cta-button connect-wallet-button"
+          onClick={connectWalletAction}
+        >
+          connect wallet
+        </button>
+      </div>
+    );
+  } else if (currentAccount && !characterNFT) {
+    return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+  } else if (currentAccount && characterNFT) {
+    return (
+      <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
+    );
+  }
+};
+
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+  
+  useEffect(() => {
+    setIsLoading(true);
+    checkIfWalletIsConnected();
+    
+    /* check network is rinkeby */
+    const checkNetwork = async () => {
+      try { 
+        if (window.ethereum.networkVersion !== '4') {
+          alert("Please connect to Rinkeby!")
+        }
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    
+  }, []);
+
+  useEffect(() => {
+  /*
+   * The function we will call that interacts with out smart contract
+   */
+  const fetchNFTMetadata = async () => {
+    console.log('Checking for Character NFT on address:', currentAccount);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    const txn = await gameContract.checkIfUserHasNFT();
+    if (txn.name) {
+      console.log('User has character NFT');
+      setCharacterNFT(transformCharacterData(txn));
+    } else {
+      console.log('No character NFT found');
+    }
+    setIsLoading(false);
+  };
+
+  /*
+   * We only want to run this, if we have a connected wallet
+   */
+  if (currentAccount) {
+    console.log('CurrentAccount:', currentAccount);
+    fetchNFTMetadata();
+  }
+}, [currentAccount]);
+
+  return (
+  <div className="App">
+    <div className="container">
+      <div className="header-container">
+        <p className="header gradient-text">"my enemies are after me"</p>
+        <p className="sub-text">can you swindle the tinder swindler? 🤡</p>
+        {/* This is where our button and image code used to be!
+         *	Remember we moved it into the render method.
+         */}
+        {renderContent()}
+      </div>
+      <div className="footer-container">
+        <p></p>
+        <a
+          className="footer-text"
+          target="_blank"
+          rel="noreferrer"
+        >{`built by sj`}</a>
+      </div>
+    </div>
+  </div>
+);
+};
+
+export default App;
---
+++ src/Components/SelectCharacter/index.jsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from 'react';
+import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
+import LoadingIndicator from "../../Components/LoadingIndicator";
+
+/*
+ * Don't worry about setCharacterNFT just yet, we will talk about it soon!
+ */
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+
+// Actions
+const mintCharacterNFTAction = async (characterId) => {
+  try {
+    if (gameContract) {
+      setMintingCharacter(true);
+      console.log('Minting character in progress...');
+      const mintTxn = await gameContract.mintCharacterNFT(characterId);
+      await mintTxn.wait();
+      console.log('mintTxn:', mintTxn);
+      setMintingCharacter(false);
+    }
+  } catch (error) {
+    console.warn('MintCharacterAction Error:', error);
+    setMintingCharacter(false);
+  }
+};
+
+// UseEffectfchoose
+useEffect(() => {
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    /*
+     * This is the big difference. Set our gameContract in state.
+     */
+    setGameContract(gameContract);
+  } else {
+    console.log('Ethereum object not found');
+  }
+}, []);
+
+useEffect(() => {
+  const getCharacters = async () => {
+    try {
+      console.log('Getting contract characters to mint');
+
+      const charactersTxn = await gameContract.getAllDefaultCharacters();
+      console.log('charactersTxn:', charactersTxn);
+
+      const characters = charactersTxn.map((characterData) =>
+        transformCharacterData(characterData)
+      );
+
+      setCharacters(characters);
+    } catch (error) {
+      console.error('Something went wrong fetching characters:', error);
+    }
+  };
+
+  /*
+   * Add a callback method that will fire when this event is received
+   */
+  const onCharacterMint = async (sender, tokenId, characterIndex) => {
+    console.log(
+      `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+    );
+
+    /*
+     * Once our character NFT is minted we can fetch the metadata from our contract
+     * and set it in state to move onto the Arena
+     */
+    if (gameContract) {
+      const characterNFT = await gameContract.checkIfUserHasNFT();
+      console.log('CharacterNFT: ', characterNFT);
+      setCharacterNFT(transformCharacterData(characterNFT));
+    }
+  };
+
+  if (gameContract) {
+    getCharacters();
+
+    /*
+     * Setup NFT Minted Listener
+     */
+    gameContract.on('CharacterNFTMinted', onCharacterMint);
+  }
+
+  return () => {
+    /*
+     * When your component unmounts, let;s make sure to clean up this listener
+     */
+    if (gameContract) {
+      gameContract.off('CharacterNFTMinted', onCharacterMint);
+    }
+  };
+}, [gameContract]);
+
+// Render Methods
+const renderCharacters = () =>
+  characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <img src={character.imageURI} alt={character.name} />
+      <button
+        type="button"
+        className="character-mint-button"
+        onClick={()=> mintCharacterNFTAction(index)}
+      >{`Mint ${character.name}`}</button>
+    </div>
+  ));
+
+  return (
+  <div className="select-character-container">
+    <h2>choose your character</h2>
+    {/* Only show this when there are characters in state */}
+    {characters.length > 0 && (
+      <div className="character-grid">{renderCharacters()}</div>
+    )}
+    {/* Only show our loading state if mintingCharacter is true */}
+    {mintingCharacter && (
+      <div className="loading">
+        <div className="indicator">
+          <LoadingIndicator />
+          <p>Minting In Progress...</p>
+        </div>
+        <img
+          src="https://www.letseatcake.com/wp-content/uploads/2022/02/Tinder-Swindler-Tweets-Memes-750x500.jpg"
+          alt="Minting loading indicator"
+        />
+      </div>
+    )}
+  </div>
+  );
+};
+
+
+export default SelectCharacter;
